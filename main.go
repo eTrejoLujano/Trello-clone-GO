b506ae28899fe442efbf3739e548558f29503cb9
Erik Trejo Lujano <trejolujano.erik@gmail.com>
2023-12-15 23:20:50 -0800

    db env var

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"database/db"
 	"database/sql"
 	"fmt"
 	"log"
@@ -10,7 +9,7 @@ import (
 	"github.com/go-sql-driver/mysql"
 )
 
-// var db *sql.DB
+var db *sql.DB
 
 type Album struct {
 	ID     int64
